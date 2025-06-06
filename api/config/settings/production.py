8f96180d87d4b0f76e9dd1d9db5ea6157ee88b0c
Hendra <hendrasyailendra@yahoo.com>
2025-06-06 12:39:23 +0800

    Server configurations

---
+++ api/config/settings/production.py
@@ -0,0 +1,17 @@
+from os import getenv, path
+
+from dotenv import load_dotenv
+
+from .base import * #noqa
+from .base import BASE_DIR
+
+local_env_file = path.join(BASE_DIR, ".envs", ".env.local")
+
+if path.isfile(local_env_file):
+    load_dotenv(local_env_file)
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = getenv("DJANGO_SECRET_KEY")
+
+ALLOWED_HOSTS = []
+ADMINS=[("Ibu kos", "hendrasyailendra@yahoo.com")]
